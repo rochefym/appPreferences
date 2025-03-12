69d13434acc7e024772e825f0b3df3a396cd7a4c
Marylle Roche <rochefym@gmail.com>
2025-03-12 15:46:45 +0800

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,74 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  isFeatureEnabled1: boolean = false;
+  isFeatureEnabled2: boolean = false;
+  isFeatureEnabled3: boolean = false;
+
+  rangeNo: number = 50;
+
+  isChecked1: boolean = false; 
+  isChecked2: boolean = false; 
+  isChecked3: boolean = false; 
+
+  selectedOption: string = '';
 
   constructor() {}
+
+  async ngOnInit() {
+    await this.loadSettings();
+  }
+
+  async saveSettings() {
+    await Preferences.set({
+      key: 'selectedOption',
+      value: this.selectedOption,
+    });
+
+    await Preferences.set({
+      key: 'rangeNo',
+      value: this.rangeNo.toString(),
+    });
+
+    await Preferences.set({
+      key: 'isChecked1',
+      value: this.isChecked1.toString(),
+    });
+  }
+
+  async loadSettings() {
+    const { value: selectedOption } = await Preferences.get({ key: 'selectedOption' });
+    const { value: rangeNo } = await Preferences.get({ key: 'rangeNo' });
+    const { value: isChecked } = await Preferences.get({ key: 'isChecked' });
+
+    if (selectedOption) {
+      this.selectedOption = selectedOption;
+    }
+
+    if (rangeNo) {
+      this.rangeNo = parseInt(rangeNo, 10);
+    }
+
+    if (isChecked) {
+      this.isChecked1 = isChecked === 'true';
+    }
+  }
+
+  resetValues() {
+    this.isFeatureEnabled1 = false;
+    this.isFeatureEnabled2 = false;
+    this.isFeatureEnabled3 = false;
+
+    this.rangeNo = 50;
+
+    this.isChecked1 = false;
+    this.isChecked2 = false;
+    this.isChecked3 = false;
+
+    this.selectedOption = ''; 
+    this.saveSettings();             
+  }
+
+
 
 }
